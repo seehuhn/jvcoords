052b0746b7806b4d256d6574544df2d58d419d1a
Jochen Voss <voss@seehuhn.de>
2021-06-04 16:50:21 +0100

    more cosmetic fixes to the unit tests

--- tests/whiten_test.R
+++ tests/whiten_test.R
@@ -21,9 +21,9 @@
 library(jvcoords)
 
 set.seed(1)
-X1 <- matrix(rnorm(990), nrow = 33, ncol = 3)
-X2 <- matrix(rnorm(990), nrow = 3, ncol = 33)
-X3 <- matrix(rnorm(999), nrow = 999, ncol = 1)
+X1 <- matrix(rnorm(99), nrow = 33, ncol = 3)
+X2 <- matrix(rnorm(99), nrow = 3, ncol = 33)
+X3 <- matrix(rnorm(99), nrow = 99, ncol = 1)
 X4 <- matrix(1:6, 3, 2)
 
 for (x in list(X1, X2, X3, X4)) {
